f3629a7b05793a00c7fb6f687ccfd4adad20a4e0
Parker Koch <pakoch@umich.edu>
2020-01-22 17:10:59 -0500

    Quick fix

--- main.py
+++ main.py
@@ -259,6 +259,8 @@ def validate(val_loader, model, criterion, args):
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    if os.path.dirname(filename) != '':
+        os.mkdirs(os.path.dirname(filename), exist_ok=True)
     torch.save(state, filename)
     if is_best:
         shutil.copyfile(filename, 'model_best.pth.tar')
